9b1f4d006ad55a6643cddda394d7800d88f95aaa
Karththigan <kpush040@uottawa.ca>
2025-11-08 21:38:37 -0500

    testworkflow1

--- src/vertex_ai/jobs/train_pipeline_job.py
+++ src/vertex_ai/jobs/train_pipeline_job.py
@@ -2,6 +2,7 @@ from google.cloud import aiplatform as aip
 from kfp.v2 import compiler
 from vertex_ai.pipelines.train_pipeline import train_pipeline
 
+# Test workflow 1
 args = {
     "bq_project_id": "belkaml",
     "bq_project_location": "US",
