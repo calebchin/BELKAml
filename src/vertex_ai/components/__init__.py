27097a23ecb628375c916113b3c5f957a65f0a0d
Caleb Chin <caleb.timothy.chin@gmail.com>
2025-11-12 12:12:42 -0500

    Add src to path to fix import error

---
+++ src/vertex_ai/components/__init__.py
@@ -0,0 +1,6 @@
+from .deploy import deploy_model_to_aip
+from .test import test_model
+from .train import train_model
+from .split import split_train_val_test_gcs
+from .preprocess import preprocess_gcs
+from .ingest import extract_bq_to_gcs
